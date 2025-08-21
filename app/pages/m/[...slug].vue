e7ab9a2fab2860953b60371e16094ab8b9bf76f4
aatrox <gnakzz@qq.com>
2025-08-21 11:49:24 +0800

    chore: eslint

--- app/pages/m/[...slug].vue
+++ app/pages/m/[...slug].vue
@@ -3,7 +3,6 @@ import type { CommentData } from '@nuxtjs/mdc'
 import type { Visitor } from '~~/types/blog'
 import type { BlogCommentWithUserInfo } from '~~/types/blog-drizzle'
 import type { ApiResponse } from '~~/types/fetch'
-import type { User } from '~~/types/memo'
 
 definePageMeta({
   layout: 'default',
