39fdb14ee9fa54a7c75114248e7670e697001683
aatrox <gnakzz@qq.com>
2025-08-26 14:05:16 +0800

    chore: rm gitea actions; github action cron

--- content.config.ts
+++ content.config.ts
@@ -15,7 +15,7 @@ export default defineContentConfig({
       },
       schema: z.object({
         date: z.date(),
-        showTitle: z.string(), // 用于展示时的文章名，title 用作生成 url 了，中文不利于 seo
+        showTitle: z.string(), // 用于展示时的文章名，title 用作生成 url 了，中文不利于 seo [已弃用此字段]
         lastmod: z.date(),
         tags: z.array(z.string()),
         versions: z.array(z.string()),
